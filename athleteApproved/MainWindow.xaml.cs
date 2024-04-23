dcd87be6aabd822b847e58f34643f750688872fe
Ice <Ice@NOT-YOUR-LAPTOP>
2024-04-24 00:56:07 +0800

    Add project files.

---
+++ athleteApproved/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace athleteApproved
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            signup objsignup = new signup();
+            this.Visibility = Visibility.Hidden;
+            objsignup.Show();
+        }
+
+
+        private void ownerClick_Click(object sender, RoutedEventArgs e)
+        {
+            logInUser objlogInUser = new logInUser();
+            this.Visibility = Visibility.Hidden;
+            objlogInUser.Show();
+        }
+
+        private void userClick_Click(object sender, RoutedEventArgs e)
+        {
+            logInOwner objlogInOwner = new logInOwner();
+            this.Visibility = Visibility.Hidden;
+            objlogInOwner.Show();
+        }
+    }
+}
---
+++ athleteApproved/signup.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace athleteApproved
+{
+    /// <summary>
+    /// Interaction logic for signup.xaml
+    /// </summary>
+    public partial class signup : Window
+    {
+        public signup()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow objMainWindow = new MainWindow();
+            this.Visibility = Visibility.Hidden;
+            objMainWindow.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainWindow objMainWindow = new MainWindow();
+            this.Visibility = Visibility.Hidden;
+            objMainWindow.Show();
+        }
+    }
+}
