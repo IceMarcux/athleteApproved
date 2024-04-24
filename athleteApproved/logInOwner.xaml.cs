1d6bd6fdb858dc487f1926eb566593e30a31fec8
ICE MARCUX CANDA <imbcanda@mymail.mapua.edu.ph>
2024-04-25 00:05:25 +0800

    pasmo

--- athleteApproved/logInOwner.xaml.cs
+++ athleteApproved/logInOwner.xaml.cs
@@ -34,7 +34,9 @@ namespace athleteApproved
 
         private void Continue_Click(object sender, RoutedEventArgs e)
         {
-
+            ownerDb objownerDb = new ownerDb();
+            this.Visibility = Visibility.Hidden;
+            objownerDb.Show();
         }
     }
 }
