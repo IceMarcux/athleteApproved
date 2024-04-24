f549c560e47981c603aeb243f47d46cf8f6d0871
ellybogs <147256103+ellybogs@users.noreply.github.com>
2024-04-24 19:30:57 +0800

    rentAgym

---
+++ athleteApproved/rentAgym.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace athleteApproved
+{
+    /// <summary>
+    /// Interaction logic for rentAgym.xaml
+    /// </summary>
+    public partial class rentAgym : Page
+    {
+        public rentAgym()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void backButton_Click(object sender, RoutedEventArgs e)
+        {
+            logInUser objMainWindow = new logInUser();
+            this.Visibility = Visibility.Hidden;
+            objMainWindow.Show();
+        }
+    }
+}
