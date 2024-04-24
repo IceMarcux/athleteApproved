d540628212b35cdde7978451b80fdfafd7d36e48
GIAN LEX MAVERICK TRINIDAD <glmdtrinidad@mymail.mapua.edu.ph>
2024-04-24 23:04:55 +0800

    commit

---
+++ athleteApproved/USER-FEEDBACK.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace athleteApproved
+{
+    /// <summary>
+    /// Interaction logic for USER_FEEDBACK.xaml
+    /// </summary>
+    public partial class USER_FEEDBACK : Window
+    {
+        public USER_FEEDBACK()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
