573fdaa3d6312bdcce4cdb860fd2c0a8a5a40952
ICE MARCUX CANDA <imbcanda@mymail.mapua.edu.ph>
2024-04-24 17:34:17 +0800

    ownerDB

---
+++ athleteApproved/ownerDb.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace athleteApproved
+{
+    /// <summary>
+    /// Interaction logic for ownerDb.xaml
+    /// </summary>
+    public partial class ownerDb : Window
+    {
+        public ownerDb()
+        {
+            InitializeComponent();
+        }
+
+        private void courtImg_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void addBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
